e3d24459bb279c45fcd085a6ed6c1b19d4333616
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-27 22:45:10 -0500

    23580, how to implement google analitics in Angular application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {Router,
+        NavigationEnd} from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+// ////esto te permite delarar el gtag objecto de google analitics
+// declare var gtag
 
 @Component({
   selector: 'app-root',
@@ -6,4 +12,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router: Router){
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+    navEndEvents$.subscribe( (event: NavigationEnd) => {
+        // gtag('config','UA-147044343', {
+          page_path: event.urlAfterRedirects
+        });
+  }
 }
