9055149236362cc12551d91cbebbfa70de2b0f30
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-29 23:08:43 -0500

    23592, how to use pure functions and memorized with pipes, this is optimizing a lot the on push strategy, this is amazing, this is the main advantage to use angular pipes

--- src/app/contact/components/list/list.component.ts
+++ src/app/contact/components/list/list.component.ts
@@ -6,13 +6,6 @@ import { Component,
          Output } from '@angular/core';
 import { EmployeeData } from 'src/app/core/models/employee.model';
 
-const fibonacci = (num: number): number => {
-  if (num === 1 || num === 2) {
-    return 1;
-  }
-  return fibonacci(num - 1) + fibonacci(num - 2);
-};
-
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -34,11 +27,6 @@ export class ListComponent implements OnInit {
   addItem(){
     this.add.emit(this.label);
     this.label = '';
-  }
-
-  calc(item: EmployeeData){
-    console.log('list', this.title);
-    return fibonacci(item.num);
   }
 
 }
