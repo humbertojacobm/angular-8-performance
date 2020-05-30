035ac74c8256c397a5db35e7d6ead9a335d88fb1
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-29 22:55:24 -0500

    23593, setting the smart and dumb component before to change the change detection estrategy

--- src/app/contact/components/list/list.component.ts
+++ src/app/contact/components/list/list.component.ts
@@ -5,6 +5,13 @@ import { Component,
          Output } from '@angular/core';
 import { EmployeeData } from 'src/app/core/models/employee.model';
 
+const fibonacci = (num: number): number => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -25,6 +32,11 @@ export class ListComponent implements OnInit {
   addItem(){
     this.add.emit(this.label);
     this.label = '';
+  }
+
+  calc(item: EmployeeData){
+    console.log('list', this.title);
+    return fibonacci(item.num);
   }
 
 }
