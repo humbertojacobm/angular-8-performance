2841781b3d4c9082a4842985d0d2264a884e7dc9
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-06-02 18:15:47 -0500

    23591, how to unsubsribe from a non httpclient observable

--- src/app/contact/components/layout/layout.component.ts
+++ src/app/contact/components/layout/layout.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+         OnDestroy,
+         OnInit } from '@angular/core';
 import { GeneratorService } from 'src/app/core/services/generator.service';
 import { EmployeeData } from 'src/app/core/models/employee.model';
+import { Observable,
+         Subscription } from 'rxjs';
 
 const names = ['nicolas','juan', 'felipe','maria'];
 
@@ -9,24 +13,31 @@ const names = ['nicolas','juan', 'felipe','maria'];
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit,
+                                        OnDestroy
+{
 
   lista1:string = "lista 1";
   lista2:string = "lista 2";
   salesList:EmployeeData[]=[];
   bList: EmployeeData[]=[];
   value: number;
+  sub$: Subscription;
 
   constructor(private generatorService: GeneratorService) { }
+  ngOnDestroy(): void {
+    console.log('destry');
+    this.sub$.unsubscribe();
+  }
 
   ngOnInit() {
     this.salesList = this.generatorService.generate(names,[10,20],10);
     this.bList = this.generatorService.generate(names,[10,20],10);
-    this.generatorService.getData()
+    this.sub$ = this.generatorService.getData()
     .subscribe(value => {
       this.value = value;
       console.log(this.value);
-    })
+    });
   }
 
   addItem(list:EmployeeData[], label: string ){
